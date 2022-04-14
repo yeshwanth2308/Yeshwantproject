bd32c91dd8fbd70318ffd6e51398cd77f78eb78b
mailt <mailt@DESKTOP-RRH8AMR>
2022-04-14 12:19:19 +0530

    TestCase 1 Updated My Yesh

--- ORANGE-HRM/src/testcases/TestCase1.java
+++ ORANGE-HRM/src/testcases/TestCase1.java
@@ -3,8 +3,11 @@ package testcases;
 public class TestCase1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		System.out.println("This is TestCase1");
+		
+		
 	}
 
 }
